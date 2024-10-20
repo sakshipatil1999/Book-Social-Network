c4aab87ae86804e3ce977756d0c10bbd3472b92a
Sakshi Patil <sakshipatil1999@gmail.com>
2024-10-19 21:40:14 -0500

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Book-Social-Network#
+# Book-Social-Network #
+
+# Overview
+Book Social Network is a full-stack application that enables users to manage their book collections and engage with a community of book enthusiasts. It offers features such as user registration, secure email validation, book management (including creation, updating, sharing, and archiving), book borrowing with checks for availability, book return functionality, and approval of book returns. The application ensures security using JWT tokens and adheres to best practices in REST API design. The backend is built with Spring Boot 3 and Spring Security 6, while the frontend is developed using Angular with Bootstrap for styling.
+
+# Features
+User Registration: Users can register for a new account.
+Email Validation: Accounts are activated using secure email validation codes.
+User Authentication: Existing users can log in to their accounts securely.
+Book Management: Users can create, update, share, and archive their books.
+Book Borrowing: Implements necessary checks to determine if a book is borrowable.
+Book Returning: Users can return borrowed books.
+Book Return Approval: Functionality to approve book returns.
+
+# Technologies:
+
+Backend: 
+SpringBoot 3
+Spring 6
+JWT token authentication
+JSR-303 and Spring Validation
+OpenAPI and Swagger UI Documentation
+Docker 
+GitHub Actions
+Keycloak
+
+Frontend:
+Angular 
+Component based architecture
+Lazy loading
+Authentication Gaurd
+OpenAPI generator for Angular
+Bootstrap framework
--- book-network/src/main/java/com/sakshi/book_network/User.java
+++ book-network/src/main/java/com/sakshi/book_network/User.java
@@ -1,4 +1,76 @@
 package com.sakshi.book_network;
 
-public class user {
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails, Principal{
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private LocalDate dateOfBirth;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private boolean accountLocked;
+    private boolean enabled;
+
+    @Override
+    public String getName() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
